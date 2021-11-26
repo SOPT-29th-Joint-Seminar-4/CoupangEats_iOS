79e2152a94dab8e922c05f6c8d86d6d8857e3c34
seondal <sseondal@gmail.com>
2021-11-26 11:30:51 +0900

    Revert "Merge branch 'develop' into feature/#4"
    This reverts commit c127db522654d39bc58fbf36985c8f9ce177c027, reversing
    changes made to dd6632f566c047af4c0962656233db4be2ad42e5.

--- CoupangEats_iOS/Source/AppDelegate.swift
+++ CoupangEats_iOS/Source/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
--- CoupangEats_iOS/Source/Extension/UIColor++.swift
+++ CoupangEats_iOS/Source/Extension/UIColor++.swift
@@ -1,28 +1,25 @@
 import UIKit
 
 extension UIColor {
-  
-  @nonobjc class var mainOrange: UIColor {
-    return UIColor(red: 237.0 / 255.0, green: 112.0 / 255.0, blue: 45.0 / 255.0, alpha: 1.0)
-  }
-  
-  @nonobjc class var titleGray: UIColor {
-    return UIColor(red: 35.0 / 255.0, green: 42.0 / 255.0, blue: 53.0 / 255.0, alpha: 1.0)
-  }
-  @nonobjc class var subtitleGray: UIColor {
-    return UIColor(red: 147.0 / 255.0, green: 157.0 / 255.0, blue: 169.0 / 255.0, alpha: 1.0)
-  }
-  
-  @nonobjc class var tagGray: UIColor {
-    return UIColor(red: 150.0 / 255.0, green: 150.0 / 255.0, blue: 150.0 / 255.0, alpha: 1.0)
-  }
-  
-  @nonobjc class var tagRed: UIColor {
-    return UIColor(red: 236.0 / 255.0, green: 101.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
-  }
-  @nonobjc class var backgroundGray: UIColor {
-    return UIColor(red: 244.0 / 255.0, green: 246.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
-  }
-  
-  
+    
+    @nonobjc class var mainOrange: UIColor {
+        return UIColor(red: 237.0 / 255.0, green: 112.0 / 255.0, blue: 45.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var titleGray: UIColor {
+        return UIColor(red: 35.0 / 255.0, green: 42.0 / 255.0, blue: 53.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var subtitleGray: UIColor {
+        return UIColor(red: 147.0 / 255.0, green: 157.0 / 255.0, blue: 169.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var tagGray: UIColor {
+        return UIColor(red: 150.0 / 255.0, green: 150.0 / 255.0, blue: 150.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var tagRed: UIColor {
+        return UIColor(red: 236.0 / 255.0, green: 101.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
+    }
+    
+    
 }
