3884d486b48328a6be8cfd4ab4fb963331954086
seondal <sseondal@gmail.com>
2021-12-03 22:52:42 +0900

    ✨ 서버.. 연결을.. 하긴 .... 했는데...

--- CoupangEats_iOS/Source/SubView/VC/SubViewVC.swift
+++ CoupangEats_iOS/Source/SubView/VC/SubViewVC.swift
@@ -20,6 +20,8 @@ class SubViewVC: UIViewController {
     
     
     //server
+    var menuData : Array<Dictionary<String,Any>>?
+    
     func getMenu() {
        let task = URLSession.shared.dataTask(with: URL(string: "https://asia-northeast3-wesopt29-e30ad.cloudfunctions.net/api/menu/")!) { (data, response, error) in
            
@@ -30,13 +32,13 @@ class SubViewVC: UIViewController {
                     let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary<String, Any>
                     
                     //Dictionary
-                    let data = json["data"] as! Array<Dictionary<String,Any>>                    
-                    for (idx, value) in data.enumerated() {
-                        if let v = value as? Dictionary <String, Any> {
-                            print("\(v["name"])")
-                        }
+                    let data = json["data"] as! Array<Dictionary<String,Any>>
+                    self.menuData = data
+                    
+                    DispatchQueue.main.async {
+                        self.TableView.reloadData()
                     }
-                }
+               }
                 catch {}
             }
         }
@@ -48,13 +50,12 @@ class SubViewVC: UIViewController {
     //viesDidLoad
   override func viewDidLoad() {
     super.viewDidLoad()
-    initContentList()
+//    initContentList()
+      getMenu()
     registerXib()
     TableView.delegate = self
     TableView.dataSource = self
     initialSet()
-      getMenu()
-    
   }
   
   func initialSet() {
@@ -92,20 +93,63 @@ extension SubViewVC: UITableViewDataSource {
   
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return contentList.count
+      
+      if let menu = menuData {
+          return menu.count
+      }
+      else {
+          return contentList.count
+      }
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       
     guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier) as? TableViewCell else { return UITableViewCell() }
       
-    if cell.ThumbnailImageView == nil {
-      print("nil이다")
-      cell.ThumbnailImageView.image = UIImage(named: "ChickenImage")
-    } else {
-      cell.ThumbnailImageView.image = UIImage(named: "ChickenImage")}
-      cell.setData(appData : contentList[indexPath.row])
-      cell.setLayout()
+      let idx = indexPath.row
+      if let menu = menuData {
+          
+          let row = menu[idx]
+          if let r = row as? Dictionary <String, Any> {
+              if let name  = r["name"] as? String {
+                  cell.StoreLabel.text = name
+              }
+              if let menu  = r["menu"] as? String {
+                  cell.MenuLabel.text = menu
+              }
+              if let likes  = r["likes"] as? Int {
+                  cell.CountLabel.text = "\(likes)명이 만족한 메뉴"
+              }
+              if let price  = r["price"] as? Int {
+                  cell.PriceLabel.text = "\(price)원"
+              }
+              if let description  = r["description"] as? String {
+                  cell.DescriptionLabel.text = description
+              }
+              if let thumbnail = r["image"] as? String {
+                  let url = URL(string: thumbnail)
+                  DispatchQueue.global().async {
+                      let data = try? Data(contentsOf: url!)
+                      DispatchQueue.main.async {
+                          cell.ThumbnailImageView.image = UIImage(data: data!)
+                      }
+                  }
+              }
+          }
+      }
+      
       return cell
+          
+//    if cell.ThumbnailImageView == nil {
+//      print("nil이다")
+//      cell.ThumbnailImageView.image = UIImage(named: "ChickenImage")
+//    } else {
+//      cell.ThumbnailImageView.image = UIImage(named: "ChickenImage")}
+//      cell.setData(appData : contentList[indexPath.row])
+//      cell.setLayout()
+//
+//      return cell
+//  }
   }
 }
+
